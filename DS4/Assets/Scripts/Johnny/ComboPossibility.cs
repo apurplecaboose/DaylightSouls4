7f939ef85b342e10ef07318220939871c26311a8
Kakiooo <johnnyjarrah@163.com>
2024-04-25 13:31:41 +0100

    importing the Sc objects and load in the data

--- DS4/Assets/Scripts/Johnny/ComboPossibility.cs
+++ DS4/Assets/Scripts/Johnny/ComboPossibility.cs
@@ -1,24 +1,32 @@
+
 using System.Collections;
 using System.Collections.Generic;
-using Unity.Burst.Intrinsics;
 using UnityEngine;
 
 public class ComboPossibility : MonoBehaviour
 {
-    [SerializeField] List<ComboType> comboTypes = new List<ComboType>();
-    [SerializeField] ComboType comboA, comboB, comboC;
-    [SerializeField] float percentageA, percentageB, percentageC,sampleCapacity;
+    [SerializeField] List<BossDataSc.ComboType> comboTypes = new List<BossDataSc.ComboType>();
+    [SerializeField] List<BossDataSc.ComboType> comboHolder = new List<BossDataSc.ComboType>();
+    [SerializeField] float sampleCapacity;
+    [SerializeField] List<float> percentageList = new List<float>();
     [SerializeField] int comboIndex;
-    [SerializeField]
-    public enum ComboType
-    {
-        A,B,C,D,E
-    }
+    public BossDataSc bossData;
     private void Awake()
     {
-        comboA = ComboType.A;
-        comboB = ComboType.B;
-        comboC = ComboType.C;
+        for (int i = 0; i < bossData.comboTypes.Count; i++)
+        {
+            comboTypes.Add(bossData.comboTypes[i]);
+            print(comboTypes[i]);
+        }
+        for (int i = 0; i < bossData.possibility.Count; i++)
+        {
+            percentageList.Add(bossData.possibility[i]);
+            print(percentageList[i]);
+        }
+        for (int i = 0; i < comboTypes.Count; i++)
+        {
+            SettingAmount(comboTypes[i], percentageList[i]);
+        }
     }
 
     // Update is called once per frame
@@ -26,24 +34,24 @@ public class ComboPossibility : MonoBehaviour
     {
     }
 
-    public void AddComboAToPool()
-    {
-        SettingAmount(comboA, percentageA);
-    }
-    public void AddComboBToPool()
-    {
-        SettingAmount(comboB, percentageB);
-    }
-    public void AddComboCToPool()
-    {
-        SettingAmount(comboC, percentageC);
-    }
+    //public void AddComboAToPool()
+    //{
+    //    SettingAmount(comboA, percentageA);
+    //}
+    //public void AddComboBToPool()
+    //{
+    //    SettingAmount(comboB, percentageB);
+    //}
+    //public void AddComboCToPool()
+    //{
+    //    SettingAmount(comboC, percentageC);
+    //}
 
     public void ShowAllValue()
     {
-        for (int i = 0; i < comboTypes.Count; i++)
+        for (int i = 0; i < comboHolder.Count; i++)
         {
-            print(comboTypes[i]);
+            print(comboHolder[i]);
         }
     }
 
@@ -51,19 +59,20 @@ public class ComboPossibility : MonoBehaviour
     {
         comboIndex = Random.Range(0, comboTypes.Count);
         print(comboTypes[comboIndex]);
-        comboTypes.Remove(comboTypes[comboIndex]);//remove the chosen number from the pool
+        //comboTypes.Remove(comboTypes[comboIndex]);//remove the chosen number from the pool
+        ////number of a's / length = a percentage
+        ////for 
+        //// if(combotypes[i] == A)
+        //// int A number ++;
 
     }
 
-    public void SettingAmount(ComboType combo, float percentage)
+    public void SettingAmount(BossDataSc.ComboType combo, float percentage)
     {
+        for (int i = 0; i < 100 * percentage; i++)
         {
-            for (int i = 0; i < 1000 * percentage; i++)
-            {
-                comboTypes.Add(combo);
-            }
+            comboHolder.Add(combo);
+            print("SHit");
         }
-      
-
     }
 }
