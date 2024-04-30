b1c4cf564d7f171b1474b6915907fac0192d9859
Kakiooo <johnnyjarrah@163.com>
2024-04-30 14:31:06 +0100

    Final possibility adapt

--- DS4/Assets/Scripts/Johnny/ComboPossibility.cs
+++ DS4/Assets/Scripts/Johnny/ComboPossibility.cs
@@ -8,7 +8,7 @@ public class ComboPossibility : MonoBehaviour
 {
     [SerializeField] List<BossDataSc.ComboType> comboTypes = new List<BossDataSc.ComboType>();
     [SerializeField] List<BossDataSc.ComboType> comboHolder = new List<BossDataSc.ComboType>();
-    [SerializeField] BossDataSc.ComboType[] resultComboArray;
+    [SerializeField] BossDataSc.ComboType[] resultComboArray, resultComboArray1, resultComboArray2, resultComboArray3, resultComboArray4, resultComboArray5, resultComboArray6, resultComboArray7;
     [SerializeField] float sampleCapacity;
     [SerializeField] List<float> percentageList = new List<float>();
     [SerializeField] int[] comboOptionIndex;
@@ -17,6 +17,13 @@ public class ComboPossibility : MonoBehaviour
     {
         comboOptionIndex = new int[3];
         resultComboArray = new BossDataSc.ComboType[3];
+        resultComboArray1 = new BossDataSc.ComboType[3];
+        resultComboArray2 = new BossDataSc.ComboType[3];
+        resultComboArray3 = new BossDataSc.ComboType[3];
+        resultComboArray4 = new BossDataSc.ComboType[3];
+        resultComboArray5 = new BossDataSc.ComboType[3];
+        resultComboArray6 = new BossDataSc.ComboType[3];
+        resultComboArray7 = new BossDataSc.ComboType[3];
 
         for (int i = 0; i < bossData.comboTypes.Count; i++)
         {
@@ -27,7 +34,7 @@ public class ComboPossibility : MonoBehaviour
             percentageList.Add(bossData.possibility[i]);
         }
 
-        for(int i = 0;i < bossData.comboTypes.Count; i++)
+        for (int i = 0; i < bossData.comboTypes.Count; i++)
         {
             AddingCombo(comboTypes[i], percentageList[i]);
         }
@@ -39,6 +46,13 @@ public class ComboPossibility : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
         {
             ChooseCombo(resultComboArray);
+            ChooseCombo(resultComboArray1);
+            ChooseCombo(resultComboArray2);
+            ChooseCombo(resultComboArray3);
+            ChooseCombo(resultComboArray4);
+            ChooseCombo(resultComboArray5);
+            ChooseCombo(resultComboArray6);
+            ChooseCombo(resultComboArray7);
         }
     }
 
@@ -52,23 +66,20 @@ public class ComboPossibility : MonoBehaviour
 
     public void ChooseCombo(BossDataSc.ComboType[] resultCombo)
     {
-        for(int i =0; i< comboOptionIndex.Length; i++)
+        for (int i = 0; i < comboOptionIndex.Length; i++)
         {
             comboOptionIndex[i] = Random.Range(0, comboHolder.Count);
             print("work");
         }
-        for( int i = 0; i< resultCombo.Length; i++)
+        for (int i = 0; i < resultCombo.Length; i++)
         {
             resultCombo[i] = comboHolder[comboOptionIndex[i]];
         }
 
     }
 
-    public void AddingCombo(BossDataSc.ComboType combo, float percentage)
+    public void AddingCombo(BossDataSc.ComboType combo, float amount)
     {
-        float amount;
-        amount = percentage * 100;//calculated before for loop
-        print(amount);
         for (int i = 0; i < amount; i++)
         {
             comboHolder.Add(combo);
