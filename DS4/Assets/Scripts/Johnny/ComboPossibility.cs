218816107993e6dcefe8c9e848237ac38a6b97c4
Kakiooo <johnnyjarrah@163.com>
2024-04-30 11:27:32 +0100

    Updating the data with array

--- DS4/Assets/Scripts/Johnny/ComboPossibility.cs
+++ DS4/Assets/Scripts/Johnny/ComboPossibility.cs
@@ -8,22 +8,23 @@ public class ComboPossibility : MonoBehaviour
 {
     [SerializeField] List<BossDataSc.ComboType> comboTypes = new List<BossDataSc.ComboType>();
     [SerializeField] List<BossDataSc.ComboType> comboHolder = new List<BossDataSc.ComboType>();
+    [SerializeField] BossDataSc.ComboType[] resultComboArray;
     [SerializeField] float sampleCapacity;
     [SerializeField] List<float> percentageList = new List<float>();
-    [SerializeField] int comboIndex;
+    [SerializeField] int[] comboOptionIndex;
     public BossDataSc bossData;
     private void Awake()
     {
+        comboOptionIndex = new int[3];
+        resultComboArray = new BossDataSc.ComboType[3];
 
         for (int i = 0; i < bossData.comboTypes.Count; i++)
         {
             comboTypes.Add(bossData.comboTypes[i]);
-            print(comboTypes[i]);
         }
         for (int i = 0; i < bossData.possibility.Count; i++)
         {
             percentageList.Add(bossData.possibility[i]);
-            print(percentageList[i]);
         }
 
         for(int i = 0;i < bossData.comboTypes.Count; i++)
@@ -35,6 +36,10 @@ public class ComboPossibility : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ChooseCombo(resultComboArray);
+        }
     }
 
     public void ShowAllValue()
@@ -45,16 +50,26 @@ public class ComboPossibility : MonoBehaviour
         }
     }
 
-    public void ChooseCombo()
+    public void ChooseCombo(BossDataSc.ComboType[] resultCombo)
     {
-        comboIndex = Random.Range(0, comboTypes.Count);
-        print(comboTypes[comboIndex]);
+        for(int i =0; i< comboOptionIndex.Length; i++)
+        {
+            comboOptionIndex[i] = Random.Range(0, comboHolder.Count);
+            print("work");
+        }
+        for( int i = 0; i< resultCombo.Length; i++)
+        {
+            resultCombo[i] = comboHolder[comboOptionIndex[i]];
+        }
+
     }
 
     public void AddingCombo(BossDataSc.ComboType combo, float percentage)
     {
-        percentage = percentage * 100;//calculated before for loop
-        for (int i = 0; i < percentage; i++)
+        float amount;
+        amount = percentage * 100;//calculated before for loop
+        print(amount);
+        for (int i = 0; i < amount; i++)
         {
             comboHolder.Add(combo);
         }
