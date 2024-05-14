6ea894b6c4f034de4a5707d177f681ec49d8c070
VVtx0v0 <2646389288@qq.com>
2024-05-14 10:20:44 +0100

    Update _TestHeavyBehavior.cs

--- DS4/Assets/Scripts/V/_TestHeavyBehavior.cs
+++ DS4/Assets/Scripts/V/_TestHeavyBehavior.cs
@@ -25,7 +25,7 @@ public class _TestHeavyBehavior : MonoBehaviour
         SelectingBossAttackState
     }
     
-    [SerializeField] int _tickCount; //Turnip:un-serialize when debug done
+    [SerializeField] int _tickCount,HeavyChargeTimer; //Turnip:un-serialize when debug done
 
     public P_Action_List P_Action;
     [SerializeField] float _P_MoveSpeed = 15f, _Ghost_MoveSpeed = 15f;
@@ -56,61 +56,57 @@ public class _TestHeavyBehavior : MonoBehaviour
                 //UNINTERUPTABLE
                 NeutralHeavyAttackAction();
                 break;
-            case P_Action_List.ChargingUpForHeavy:
+            case P_Action_List.ChargingUpForHeavy: ChargingUpHeavyAction();
                 // if held for too long change state to held heavy (also set the time held varible to max as input)
                 break;
             case P_Action_List.ChargedHeavy:
-                ChargedHeavyAttackAction();
+                
                 break;
 
         }
 
     }
-    public void NeutralHeavyAttackInput(InputAction.CallbackContext inputState)
+    public void HeavyAttackInput(InputAction.CallbackContext inputState)
     {
+        int holdpoint = 60;
+        
+        
+        /// if(preformed)
+        /// sometimer += sometime either use delta time or fixed update ints
+        ///     if(sometimer >= holdpoint)
+        ///        state = chargingupforheavy
+        ///        
+        /// if(canceled)
+        ///     if(state != chargingupforheavy)
+        ///         state = NeutralHeavy
+        ///     else //you were charging
+        ///         state = chargedheavy 
+        ///         give the charge time in a varible here
+        ///         
+        
         if (inputState.performed)
         {
-            if (P_Action == P_Action_List.NULL_ACTION_STATE)
+            HeavyChargeTimer+=1;
+            if (HeavyChargeTimer > holdpoint)
             {
-                P_Action = P_Action_List.NeutralHeavy;
-                Debug.Log("NeutralHeavy");
+                P_Action = P_Action_List.ChargingUpForHeavy;
+                
             }
-            else if(P_Action == P_Action_List.ChargingUpForHeavy)
+        }
+        if (inputState.canceled)
+        {
+            if (P_Action != P_Action_List.ChargingUpForHeavy)
             {
                 P_Action = P_Action_List.NeutralHeavy;
-                Debug.Log("NeutralHeavy");
             }
             else
             {
-                Debug.Log("Busy!");
-                return;
-            }
-        }
-    }
-    public void HoldHeavyAttackInput(InputAction.CallbackContext inputState)
-    {
-        if (P_Action == P_Action_List.NULL_ACTION_STATE)
-        {
-            if (inputState.started)
-            {
-                P_Action = P_Action_List.ChargingUpForHeavy;
-            }
-
-        }
-        if (P_Action == P_Action_List.ChargingUpForHeavy)
-        {
-            if (inputState.canceled)
-            {
-                P_Action = P_Action_List.NeutralHeavy;
-                chargeCancelled = inputState.canceled;
-            }
-            if (inputState.performed)
-            {
                 P_Action = P_Action_List.ChargedHeavy;
-                Debug.Log("ChargedHeavy");
+
             }
         }
     }
+
     void LightAttackAction()
     {
         //swap player UNINTERUPTABLE
@@ -143,13 +139,9 @@ public class _TestHeavyBehavior : MonoBehaviour
             _tickCount += 1;
         }
     }
-    void ChargingHeavyAttackAction()//play charge animation, not able to move, disruptable by swap
+    void ChargingUpHeavyAction()//play charge animation, not able to move, disruptable by swap
     {
-        //seems vague because the input function handeled the state switch well enough.
-        if (chargeCancelled)//catch case?
-        {
-            P_Action = P_Action_List.NeutralHeavy;
-        }
+        
 
 
     }
@@ -184,45 +176,12 @@ public class _TestHeavyBehavior : MonoBehaviour
                 case int t when t >= totalTicks:
                     P_Action = P_Action_List.NULL_ACTION_STATE;// catch case
                     _tickCount = 0;
+                    HeavyChargeTimer = 0;
                     break;
             }
             _tickCount += 1;
         }
     }
-    void ChargedHeavyAttackAction()
-    {
-        //swap player UNINTERUPTABLE
-        int startUpFrames = 5;
-
-
-        int activeFrames = 5;
-
-        int recoveryFrames = 10;
-        int totalTicks = startUpFrames + activeFrames + recoveryFrames;
-
-        if (_tickCount >= totalTicks)
-        { //Turnip: done ticking reset back to null action state and set tickcount back to 0 for next action
-            P_Action = P_Action_List.NULL_ACTION_STATE;
-            _tickCount = 0;
-            return;
-        }
-        else
-        { // Turnip: run logic 
-            switch (_tickCount)
-            {
-                case int t when t < startUpFrames://startup phase
-                    break;
-                case int t when t >= startUpFrames && t < startUpFrames + activeFrames://play swing animation&check if damage dealt 
-                    break;
-                case int t when t >= startUpFrames + activeFrames && t < totalTicks://play recover animation
-                    break;
-                case int t when t >= totalTicks:
-                    P_Action = P_Action_List.NULL_ACTION_STATE;// catch case
-                    _tickCount = 0;
-                    break;
-            }
-            _tickCount += 1;
-        }
-    }
+ 
 
 }
