99a3b55a6640869eb9c31a2498c565546822702e
VVtx0v0 <2646389288@qq.com>
2024-05-16 19:32:38 +0100

    BossHP slider functional

--- DS4/Assets/Scripts/V/BossHealthUI/BossHeathbarTestBehavior.cs
+++ DS4/Assets/Scripts/V/BossHealthUI/BossHeathbarTestBehavior.cs
@@ -1,9 +1,88 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class BossHeathbarTestBehavior : MonoBehaviour
-{
-   
+{// written intended to be attactch to Boss Object
+    public int BossHp,PlayerDamage;
+    float _stackCountDwonTimer;
+    int _stackedDamage;
+
+    SpriteRenderer _bossSR;
+    [SerializeField]
+    TMP_Text _bossHp;
+    [SerializeField]
+    Slider _bossHP_Slider;
+
+    
+    void Start()
+    {
+        _bossSR = this.GetComponent<SpriteRenderer>();
+    }
+   void Update()
+    {
+        DamageStackDisplay();
+    }
+    private void OnTriggerEnter2D(Collider2D bossCollider)
+    {
+        if (bossCollider.CompareTag("LightAttack"))
+        {
+            Debug.Log("collided");
+            BossHp -= PlayerDamage;
+            _bossSR.color = Color.red;
+            _bossHP_Slider.value = BossHp;
+
+            _stackedDamage += PlayerDamage;
+            _stackCountDwonTimer += FrameToSec(18f+ 5f) + FrameToSec(500f / _stackedDamage);
+        }
+        if (bossCollider.CompareTag("HeavyAttack"))
+        {
+            BossHp -= PlayerDamage*2;
+            _bossSR.color = Color.red;
+            _bossHP_Slider.value = BossHp;
+
+            _stackedDamage += PlayerDamage;
+            _stackCountDwonTimer += FrameToSec(18f+ 5f) + FrameToSec(500f / _stackedDamage);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D bossCollider)
+    {
+        if (bossCollider.CompareTag("LightAttack"))
+        {
+            _bossSR.color = Color.white;
+        }
+        if (bossCollider.CompareTag("HeavyAttack"))
+        {
+            _bossSR.color = Color.white;
+        }
+    }
+    void DamageStackDisplay()
+    {
+        if (_stackCountDwonTimer > 2f)
+        {
+            _bossHp.text = _stackedDamage.ToString();
+            _stackCountDwonTimer -= 2 * Time.deltaTime;
+        }
+        else if (_stackCountDwonTimer > 0)
+        {
+            _bossHp.text = _stackedDamage.ToString();
+            _stackCountDwonTimer -= Time.deltaTime;
+        }
+        else
+        {
+            _bossHp.text = "";
+            _stackedDamage = 0;
+            _stackCountDwonTimer = 0;
+
+        }
+    }
+
+    float FrameToSec(float frames)
+    {
+        frames *= 1 / 60f;
+        return frames;
+    }
 }
