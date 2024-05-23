f757a83377de701dbcba7a109b7c7e9364b90ec3
Kakiooo <johnnyjarrah@163.com>
2024-05-23 14:23:08 +0100

    Player_HealthBar_Remake

---
+++ DS4/Assets/Scripts/Johnny/Player_HealthBar.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player_HealthBar : MonoBehaviour
+{
+    public float HealthValue = 100, ShieldValue;
+    float _ShieldOriginalValue, _HealthOriginalValue;
+    public float DamageTaken;
+    public Image ShieldImage, HealthImage;
+    private void Awake()
+    {
+        ShieldValue = HealthValue;
+        _ShieldOriginalValue = ShieldValue;
+        _HealthOriginalValue = HealthValue;
+    }
+
+    private void Update()
+    {
+    }
+
+    public void RestoreHealth()
+    {
+        float restoreSpeed = 10;
+        ShieldValue = Mathf.Clamp(ShieldValue, 0, HealthValue);
+        ShieldValue += restoreSpeed * Time.deltaTime;
+        float t = Mathf.InverseLerp(0, _ShieldOriginalValue, ShieldValue);
+        ShieldImage.fillAmount = t;
+    }
+    public void DamageLogic(float Damage)
+    {
+        ShieldDamageTaken(Damage);
+        if (ShieldImage.fillAmount == 0 && HealthImage.fillAmount > 0)
+        {
+            HealthDamageTaken(Damage);
+        }
+    }
+
+    public void ShieldDamageTaken(float damage)
+    {
+        float currentValue = ShieldValue;
+        currentValue -= damage;
+        float t = Mathf.InverseLerp(0, _ShieldOriginalValue, currentValue);
+        ShieldImage.fillAmount = t;
+        ShieldValue = currentValue;
+    }
+    public void HealthDamageTaken(float damage)
+    {
+        float currentValue = HealthValue;
+        currentValue -= damage;
+        float t = Mathf.InverseLerp(0, _HealthOriginalValue, currentValue);
+        HealthImage.fillAmount = t;
+        HealthValue = currentValue;
+    }
+
+}
