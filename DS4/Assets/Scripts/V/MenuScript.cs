2e10d28b931a4fd593d8499f32680a3fdbfcf688
VVtx0v0 <2646389288@qq.com>
2024-05-27 16:09:25 +0100

    Update MenuScript.cs

--- DS4/Assets/Scripts/V/MenuScript.cs
+++ DS4/Assets/Scripts/V/MenuScript.cs
@@ -1,25 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.SceneManagement;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MenuScript : MonoBehaviour
 {
-    public int GameSceneIndex,TutorialSceneIndex,NextPageIndex,MenuIndex ;
+    [Header("Required Fields MUST FILL!!!")]
+    [Header("set index number according to build settings and script will jump to the desired scene")]
+    public int GameSceneIndex,TutorialSceneStartIndex,MenuIndex;
+    int _TutorialCurrentPageIndex,_NextPageIndex;
+
     public void StartGame()
     {
         SceneManager.LoadScene(GameSceneIndex);
     }
-    public void StatTutorial()
+    public void StartTutorial()
     {
-        SceneManager.LoadScene(0);
+        SceneManager.LoadScene(TutorialSceneStartIndex);
     }
     public void NextPage()
     {
-        SceneManager.LoadScene(0);
+        SceneManager.LoadScene(_NextPageIndex);
     }
     public void BackToMenu()
     {
-        SceneManager.LoadScene(0);
+        SceneManager.LoadScene(MenuIndex);
+    }
+    private void Awake()
+    {
+        _TutorialCurrentPageIndex = TutorialSceneStartIndex;
+    }
+    void Update()
+    {
+        if (_NextPageIndex! > 10)
+        {
+          _NextPageIndex = _TutorialCurrentPageIndex+1;
+        }
+        if (_NextPageIndex >= 10)
+        {
+            BackToMenu();
+        }
+
     }
 }
