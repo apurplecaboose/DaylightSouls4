679a9b3b41cee30549aa2653504552c189f84428
VVtx0v0 <2646389288@qq.com>
2024-05-27 16:50:31 +0100

    set up new animation, cleaned menu code

--- DS4/Assets/Scripts/V/MenuScript.cs
+++ DS4/Assets/Scripts/V/MenuScript.cs
@@ -7,41 +7,27 @@ using UnityEngine.SceneManagement;
 
 public class MenuScript : MonoBehaviour
 {
-    [Header("Required Fields MUST FILL!!!")]
-    [Header("set index number according to build settings and script will jump to the desired scene")]
-    public int GameSceneIndex,TutorialSceneStartIndex,MenuIndex;
-    int _TutorialCurrentPageIndex,_NextPageIndex;
-
     public void StartGame()
     {
-        SceneManager.LoadScene(GameSceneIndex);
+        int scenenumb = 1;
+        SceneManager.LoadScene(scenenumb);
     }
     public void StartTutorial()
     {
-        SceneManager.LoadScene(TutorialSceneStartIndex);
+        int scenenumb = 1;
+        SceneManager.LoadScene(scenenumb);
     }
     public void NextPage()
     {
-        SceneManager.LoadScene(_NextPageIndex);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void PreviousPage()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex -1);
     }
     public void BackToMenu()
     {
-        SceneManager.LoadScene(MenuIndex);
-    }
-    private void Awake()
-    {
-        _TutorialCurrentPageIndex = TutorialSceneStartIndex;
-    }
-    void Update()
-    {
-        if (_NextPageIndex! > 10)
-        {
-          _NextPageIndex = _TutorialCurrentPageIndex+1;
-        }
-        if (_NextPageIndex >= 10)
-        {
-            BackToMenu();
-        }
-
+        int scenenumb = 1;
+        SceneManager.LoadScene(scenenumb);
     }
 }
